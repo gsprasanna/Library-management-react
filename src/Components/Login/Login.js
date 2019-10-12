787fdc9e2e22497901e0dee95de72041201e5a16
Prasanna <prasanna@Prasannas-MacBook-Pro.local>
2019-10-12 17:28:57 +0530

    added login component

---
+++ src/Components/Login/Login.js
@@ -0,0 +1,133 @@
+import React, { Component } from "react";
+
+import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
+import Avatar from "@material-ui/core/Avatar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+
+const useStyles = makeStyles(theme => ({
+  "@global": {
+    body: {
+      backgroundColor: theme.palette.common.white
+    }
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  }
+}));
+
+const Login = ({
+  users,
+  isAuthenticated,
+  onUsernameChange,
+  onPasswordChange,
+  checkLogin,
+  customInputLabel,
+  customInputPlaceholder,
+  customPasswordLabel,
+  customPasswordPlaceholder
+}) => {
+  const classes = useStyles();
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} onSubmit={checkLogin}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label={customInputLabel}
+            placeholder={customInputPlaceholder}
+            name="username"
+            autoComplete="username"
+            autoFocus
+            onChange={onUsernameChange}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label={customPasswordLabel}
+            placeholder={customPasswordPlaceholder}
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            onChange={onPasswordChange}
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+        </form>
+      </div>
+    </Container>
+  );
+};
+
+export default Login;
+
+// <div>
+//       <Form onSubmit={checkLogin}>
+//         <FormGroup>
+//           <Label for="Username">{customInputLabel}</Label>
+//           <Input
+//             type="text"
+//             name="username"
+//             placeholder={customInputPlaceholder}
+//             onChange={onUsernameChange}
+//           />
+//         </FormGroup>
+//         <FormGroup>
+//           <Label for="examplePassword">{customPasswordLabel}</Label>
+//           <Input
+//             type="password"
+//             name="password"
+//             id="examplePassword"
+//             placeholder={customPasswordPlaceholder}
+//             onChange={onPasswordChange}
+//           />
+//         </FormGroup>
+//         <Button className="btn btn-primary">Sign In</Button>
+//       </Form>
+//     </div>
--- src/Constants/ServerUrl.js
+++ src/Constants/ServerUrl.js
@@ -1,3 +1,7 @@
 export const PRIMARY_SERVER = "http://localhost:3000";
 
 export const GET_BOOKS = "/data";
+
+export const POST_BOOKS = "/data";
+
+export const GET_USERS = "/users";
